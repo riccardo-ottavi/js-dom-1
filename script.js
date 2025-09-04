657d852edc38797bcce8db00f689daec117b5d5e
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-04 15:41:36 +0200

    migliora allienamento

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ const mainBox = document.getElementById("box");
 //Aggiungi evento di cambio immagine
 mainBtn.addEventListener("click", function(){
     lampImg.src = "img/yellow_lamp.png";
-    mainBox.style.gap = "40px";
+    mainBox.style.gap = "38px";
     mainBox.style.marginLeft = "39vw";
 });
