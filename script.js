246e25221df0e18469bdd038c861658d42d913fd
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-04 14:53:13 +0200

    inizializza le costanti per gli elementi necessari

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("prova");
+//Inizializza le costanti relative agli elementi
+const lampImg = document.getElementById("myImg");
+const mainBtn = document.getElementById("myBtn");
+//Aggiungi evento di cambio immagine
