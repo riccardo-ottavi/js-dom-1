7b2fd1deb682bf38ef250dc3df3fc6240539d34e
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-04 15:27:18 +0200

    correggi spostamento lampadina

--- bonus/script.js
+++ bonus/script.js
@@ -1,12 +1,7 @@
 //Inizializza le costanti relative agli elementi
 const lampImg = document.getElementById("myImg");
 const mainBtn = document.getElementById("myBtn");
-
-const mainBox = document.getElementById("box");
-
 //Aggiungi evento di cambio immagine
 mainBtn.addEventListener("click", function(){
     lampImg.src = "img/yellow_lamp.png";
-    mainBox.style.gap = "40px";
-    mainBox.style.marginLeft = "39vw";
 });
