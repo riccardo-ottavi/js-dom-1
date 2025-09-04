65db6723298b784c028d9ca80ab88b7ec6a18c1e
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-04 15:55:45 +0200

    inizializza bonus

--- bonus/script.js
+++ bonus/script.js
@@ -1,7 +1,12 @@
 //Inizializza le costanti relative agli elementi
 const lampImg = document.getElementById("myImg");
 const mainBtn = document.getElementById("myBtn");
+
+const mainBox = document.getElementById("box");
+
 //Aggiungi evento di cambio immagine
 mainBtn.addEventListener("click", function(){
     lampImg.src = "img/yellow_lamp.png";
+    mainBox.style.gap = "38px";
+    mainBox.style.marginLeft = "39vw";
 });
