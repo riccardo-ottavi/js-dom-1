a53f7f083fcba80a2b423860078cceaa29c9d47c
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-04 16:17:22 +0200

    predisponi toggle e booleano per verificarre stato

--- bonus/script.js
+++ bonus/script.js
@@ -1,13 +1,26 @@
 //Inizializza le costanti relative agli elementi
 const lampImg = document.getElementById("myImg");
 const mainBtn = document.getElementById("myBtn");
-
 const mainBox = document.getElementById("box");
 
+let isLightOn = false;
+
+mainBtn.addEventListener("click", toggleLight());
+
+
+
 //Aggiungi evento di cambio immagine
-mainBtn.addEventListener("click", function(){
+
+function toggleLight {
+
+}
+
+
+function lightOn(){
     lampImg.src = "img/yellow_lamp.png";
     mainBox.style.gap = "38px";
     mainBox.style.marginLeft = "39vw";
     mainBtn.innerText = "Spegni";
-});
+    console.log("hai acceso la luce");
+};
+
