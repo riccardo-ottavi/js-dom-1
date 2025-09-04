faaea55d1dbea9498d9b13dd78d3394f49249c94
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-04 16:26:03 +0200

    inserisci effetto toggle

--- bonus/script.js
+++ bonus/script.js
@@ -5,22 +5,35 @@ const mainBox = document.getElementById("box");
 
 let isLightOn = false;
 
-mainBtn.addEventListener("click", toggleLight());
+mainBtn.addEventListener("click", toggleLight);
 
 
 
 //Aggiungi evento di cambio immagine
 
-function toggleLight {
+function toggleLight() {
+    if (isLightOn === false) {
+        lampImg.src = "img/yellow_lamp.png";
+        mainBox.style.gap = "38px";
+        mainBox.style.marginLeft = "39vw";
+        mainBtn.innerText = "Spegni";
+        console.log("hai acceso la luce");
+        
 
+    }else if (isLightOn=== true) {
+        lampImg.src = "img/white_lamp.png";
+        mainBox.style.gap = "38px";
+        mainBox.style.marginLeft = "39vw";
+        mainBtn.innerText = "Accendi";
+        console.log("hai acceso la luce");
+        
+    };
+    if (isLightOn === true) {
+        isLightOn = false; 
+    }else if (isLightOn === false){
+        isLightOn = true;
+    }
 }
 
 
-function lightOn(){
-    lampImg.src = "img/yellow_lamp.png";
-    mainBox.style.gap = "38px";
-    mainBox.style.marginLeft = "39vw";
-    mainBtn.innerText = "Spegni";
-    console.log("hai acceso la luce");
-};
 
